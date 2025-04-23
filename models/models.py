61f705c894fc2d0fc6a7bf52b4f5c870c4ab2ec9
rezvaya <rezvaya2016@outlook.com>
2025-04-23 20:15:29 +0300

    add owner field to url model

--- models/models.py
+++ models/models.py
@@ -1,10 +1,12 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional
 import uuid
 
 class URL(SQLModel, table=True):
     id: str = Field(default_factory=lambda: uuid.uuid4().hex[:6], primary_key=True)
     original_url: str
-    clicks: int = 0 
+    clicks: int = 0
+    owner: str 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
