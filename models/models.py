9f69f96e379adffc654bf4dffee6a4c5bd97f02b
rezvaya <rezvaya2016@outlook.com>
2025-04-23 19:46:33 +0300

    add user model

--- models/models.py
+++ models/models.py
@@ -5,3 +5,8 @@ class URL(SQLModel, table=True):
     id: str = Field(default_factory=lambda: uuid.uuid4().hex[:6], primary_key=True)
     original_url: str
     clicks: int = 0 
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    hashed_password: str
