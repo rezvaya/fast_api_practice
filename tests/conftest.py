08e065f99a4dc519b1b128c1582decd082fd73d6
rezvaya <rezvaya2016@outlook.com>
2025-04-30 19:51:55 +0300

    fixed test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from app.main import app
 from app.db.session import get_session
 
 DB_URL = 'sqlite:///./test.db'
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 
 def override_get_session():
     with Session(engine) as session:
@@ -15,6 +15,19 @@ def override_get_session():
 
 app.dependency_overrides[get_session] = override_get_session
 
+@pytest.fixture(scope="module", autouse=True)
+def prepare_database():
+    SQLModel.metadata.create_all(engine)
+    yield
+    SQLModel.metadata.drop_all(engine)
+
+
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def user():
+    class FakeUser:
+        username="test_user"
+    return FakeUser()
